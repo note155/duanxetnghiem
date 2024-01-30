c51a359281d5a4eedffd36460d8f541d5d5d49a9
Windows 10 <Windows 10@21AK22-COM>
2024-01-30 10:50:50 +0700

    Services

--- duanxetnghiem/duanxetnghiem.Client/Program.cs
+++ duanxetnghiem/duanxetnghiem.Client/Program.cs
@@ -10,6 +10,10 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 builder.Services.AddScoped<IUser, UserServices>();
+builder.Services.AddScoped<IBacSi, BacSiServices>();
+builder.Services.AddScoped<IDonXetNghiem, DonXetNghiemServices>();
+builder.Services.AddScoped<IGoiXetNghiem, GoiXetNghiemServices>();
+builder.Services.AddScoped<IKetQuaXetNghiem, KetQuaXetNghiemServices>();
 builder.Services.AddScoped(http => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
--- duanxetnghiem/duanxetnghiem.Client/Services/BacSiServices.cs
+++ duanxetnghiem/duanxetnghiem.Client/Services/BacSiServices.cs
@@ -1,9 +1,10 @@
 ﻿using duanxetnghiem.Data.Model;
+using Shared.ketnoi;
 using System.Net.Http.Json;
 
 namespace duanxetnghiem.Client.Services
 {
-    public class BacSiServices
+    public class BacSiServices :IBacSi
     {
         private readonly HttpClient _httpClient;
         public BacSiServices(HttpClient httpClient)
@@ -17,28 +18,28 @@ namespace duanxetnghiem.Client.Services
             return respone;
         }
 
-        public async Task<BacSi> deleteuserAsync(int id)
+        public async Task<BacSi> deleteAsync(int id)
         {
             var newstudent = await _httpClient.PostAsJsonAsync("api/BacSi/Delete-BacSi", id);
             var respone = await newstudent.Content.ReadFromJsonAsync<BacSi>();
             return respone;
         }
 
-        public async Task<List<BacSi>> getalluserAsync()
+        public async Task<List<BacSi>> getallAsync()
         {
             var allstudent = await _httpClient.GetAsync("api/BacSi/All-BacSi");
             var respone = await allstudent.Content.ReadFromJsonAsync<List<BacSi>>();
             return respone;
         }
 
-        public async Task<BacSi> getuserbyid(int Id)
+        public async Task<BacSi> getbyid(int Id)
         {
             var onestudent = await _httpClient.GetAsync("api/BacSi/Single-BacSi");
             var respone = await onestudent.Content.ReadFromJsonAsync<BacSi>();
             return respone;
         }
 
-        public async Task<BacSi> updateuserAsync(BacSi User)
+        public async Task<BacSi> updateAsync(BacSi User)
         {
             var newstudent = await _httpClient.PostAsJsonAsync("api/BacSi/Update-BacSi", User);
             var respone = await newstudent.Content.ReadFromJsonAsync<BacSi>();
